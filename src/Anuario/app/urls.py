f8eb4628171f6407f1e5e12eadeeb888b29f6806
MildredMtz <mildred27@ciencias.unam.mx>
2025-05-13 15:26:40 -0600

    Funciones para perfil, edición de perfil y grupos

--- src/Anuario/app/urls.py
+++ src/Anuario/app/urls.py
@@ -4,8 +4,11 @@ from . import views
 
 urlpatterns = [
     path("nominaciones/", views.nominaciones, name="nominaciones"),
-    path("verNominacion/<str:idNominacion>", views.verNominacion, name="verNominacion")
+    path("verNominacion/<str:idNominacion>", views.verNominacion, name="verNominacion"),
     #path("551123012/", views.index, name="index"),
+    path("perfil/", views.verPerfil, name="perfil"),
+    path("perfil/editar/", views.editar_perfil, name="editar_perfil"),
+    path("grupo/info/", views.detalle_grupo, name="grupo_info"),
     path("accounts/signup/", views.signup, name="signup"),
     path("accounts/", include("django.contrib.auth.urls")),  # new
     path("", TemplateView.as_view(template_name="home.html"), name="home")  # new
