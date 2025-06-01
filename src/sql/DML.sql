c7f6d32d3bcaa8a593c1b9c5d12c609d851deb5e
Leslie SanV <111658258+paolasanv@users.noreply.github.com>
2025-06-01 16:07:41 -0600

    Actualización DML

--- src/sql/DML.sql
+++ src/sql/DML.sql
@@ -3,51 +3,31 @@
 	Son los premios por ganar una nominación.
 */
 
-insert into app_marco ("marco")
-values ('marco/Marco1.png');
-
-insert into app_marco ("marco")
-values ('marco/Marco2.png');
-
-insert into app_marco ("marco")
-values ('marco/Marco3.png');
-
-insert into app_marco ("marco")
-values ('marco/Marco4.png');
-
-insert into app_marco ("marco")
-values ('marco/Marco5.png');
-
-insert into app_marco ("marco")
-values ('marco/Marco6.png');
-
-insert into app_marco ("marco")
-values ('marco/Marco7.png');
-
-insert into app_marco ("marco")
-values ('marco/Marco8.png');
+INSERT INTO app_marco ("marco", "es_multiple")
+VALUES 
+  ('marco/Marco1.png', false),
+  ('marco/Marco2.png', false),
+  ('marco/Marco3.png', false),
+  ('marco/Marco4.png', false),
+  ('marco/Marco5.png', false),
+  ('marco/Marco6.png', false),
+  ('marco/Marco7.png', false),
+  ('marco/Marco8.png', false);
 
 
 /*
 	Inserta nominaciones.
 */
-INSERT INTO app_nominacion (categoria, descripcion, activa, mostrar_resultados)
-VALUES ('Más Participativo', 'Reconocimiento al alumno que mas ha participado en el grupo.', true, True);
-
-INSERT INTO app_nominacion (categoria, descripcion, activa, mostrar_resultados)
-VALUES ('Más Gracioso', 'Reconocimiento al alumno con los mejores chistes en el grupo.', true, True);
-
-INSERT INTO app_nominacion (categoria, descripcion, activa, mostrar_resultados)
-VALUES ('Mejor Peinado', 'Reconocimiento al alumno con el mejor estilo de cabello.', true, True);
-
-INSERT INTO app_nominacion (categoria, descripcion, activa, mostrar_resultados)
-VALUES ('Mejor Proyecto', 'Reconocimiento al mejor proyecto del año.', true, True);
-
-INSERT INTO app_nominacion (categoria, descripcion, activa, mostrar_resultados)
-VALUES ('Líder Inspirador', 'Reconocimiento al alumno por liderazgo destacado.', true, True);
-
-INSERT INTO app_nominacion (categoria, descripcion, activa, mostrar_resultados)
-VALUES ('Innovador del Año', 'Reconocimiento al alumno con ideas fuera de lo común.', true, True);
+INSERT INTO app_nominacion (categoria, descripcion, activa, mostrar_resultados, "premio_id")
+VALUES 
+  ('Más Participativo', 'Reconocimiento al alumno que mas ha participado en el grupo.', true, true, 1),
+  ('Más Gracioso', 'Reconocimiento al alumno con los mejores chistes en el grupo.', true, true, 2),
+  ('Mejor Peinado', 'Reconocimiento al alumno con el mejor estilo de cabello.', true, true, 3),
+  ('Mejor Proyecto', 'Reconocimiento al mejor proyecto del año.', true, true, 4),
+  ('Líder Inspirador', 'Reconocimiento al alumno por liderazgo destacado.', true, true, 5),
+  ('Innovador del Año', 'Reconocimiento al alumno con ideas fuera de lo común.', true, true, 6),
+  ('Compañero Ideal', 'Reconocimiento al alumno que siempre está dispuesto a ayudar a los demás.', true, true, 7),
+  ('Espíritu Escolar', 'Reconocimiento al alumno que representa mejor los valores del grupo.', true, true, 8);
 
 
 /*
@@ -58,49 +38,15 @@ VALUES ('Innovador del Año', 'Reconocimiento al alumno con ideas fuera de lo co
 	de cada tupla con un número n, donde 1 ≤ n ≤ #total de grupos creados como administrador.
 
  */
-insert into app_existe("idNominacion_id", codigo_id, fecha_inicio, fecha_fin)
-values (1, 1, '2025-05-01 08:00:00', '2025-07-01 23:59:59');
-
-insert into app_existe("idNominacion_id", codigo_id, fecha_inicio, fecha_fin)
-values (2, 1, '2025-05-01 08:00:00', '2025-07-01 23:59:59');
-
-insert into app_existe("idNominacion_id", codigo_id, fecha_inicio, fecha_fin)
-values (3, 1, '2025-05-01 08:00:00', '2025-07-01 23:59:59');
-
-insert into app_existe("idNominacion_id", codigo_id, fecha_inicio, fecha_fin)
-values (4, 1, '2025-05-01 08:00:00', '2025-07-01 23:59:59');
-
-insert into app_existe("idNominacion_id", codigo_id, fecha_inicio, fecha_fin)
-values (5, 1, '2025-05-01 08:00:00', '2025-07-01 23:59:59');
-
-insert into app_existe("idNominacion_id", codigo_id, fecha_inicio, fecha_fin)
-values (6, 1, '2025-05-01 08:00:00', '2025-07-01 23:59:59');
+INSERT INTO app_existe("idNominacion_id", codigo_id, fecha_inicio, fecha_fin)
+VALUES 
+  (1, 1, '2025-05-01 08:00:00', '2025-07-01 23:59:59'),
+  (2, 1, '2025-05-01 08:00:00', '2025-07-01 23:59:59'),
+  (3, 1, '2025-05-01 08:00:00', '2025-07-01 23:59:59'),
+  (4, 1, '2025-05-01 08:00:00', '2025-07-01 23:59:59'),
+  (5, 1, '2025-05-01 08:00:00', '2025-07-01 23:59:59'),
+  (6, 1, '2025-05-01 08:00:00', '2025-07-01 23:59:59'),
+  (7, 1, '2025-05-01 08:00:00', '2025-07-01 23:59:59'),
+  (8, 1, '2025-05-01 08:00:00', '2025-07-01 23:59:59');
 
 
-/*
-	Asignación de Premios. 
-
-	Reemplace el carácter '_' con el número de cuenta del usuario al que desee 
-	asignarle un marco de perfil en la aplicación, con el fin de permitirle visualizar 
-	los marcos de perfil sin necesidad de haber ganado una nominación.
-*/
-
-/*
-INSERT INTO app_ganar ("premio_id", "activa", "idNominacion_id", "numCuenta_id" )
-VALUES (5, true, 1, _ );
-
-INSERT INTO app_ganar ("premio_id", "activa", "idNominacion_id", "numCuenta_id" )
-VALUES (7, true, 2, _ );
-
-INSERT INTO app_ganar ("premio_id", "activa", "idNominacion_id", "numCuenta_id" )
-VALUES (6, true, 3, _ );
-
-INSERT INTO app_ganar ("premio_id", "activa", "idNominacion_id", "numCuenta_id" )
-VALUES (2, true, 4, _ );
-
-INSERT INTO app_ganar ("premio_id", "activa", "idNominacion_id", "numCuenta_id" )
-VALUES (3, true, 5, _ );
-
-INSERT INTO app_ganar ("premio_id", "activa", "idNominacion_id", "numCuenta_id" )
-VALUES (8, true, 6, _ );
-*/
